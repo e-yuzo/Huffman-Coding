191c8e43c70f591e255a4ad8e215999f190f9d44
Eduardo Yuzo Nakai <yuzonakai@gmail.com>
2017-05-25 21:11:07 -0300

    mais metodos

--- HuffmanCoding/include/BNode.h
+++ HuffmanCoding/include/BNode.h
@@ -6,16 +6,29 @@ class BNode
 {
     public:
         BNode();
-        BNode();
-        virtual ~BNode();
+        BNode(bool leaf); //set atributo que verifica se é folha
 
+        void setValue(char c);
+        void setValue(int n);
+
+        int getValueN();
+        char getValueC();
+
+        virtual ~BNode();
     protected:
 
     private:
         BNode* left;
         BNode* right;
 
+        char c;
+        int n;
+
         bool leaf; //verifica se é folha
+
+        void caractereCount(); //define frequência
+        void readFile(); //lê arquivo
+        void writeFile(); //escreve no arquivo
 };
 
 #endif // BNODE_H
